e7525c427d92a170af83667e35a8c6d78d8b7526
Nekohime <kuroneko@protonmail.com>
2020-05-10 00:44:37 +0300

    qwe

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var q = 0;
+            while (true)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine($"Hello World!{q++}");
+            }
+        }
+    }
+    
+}
